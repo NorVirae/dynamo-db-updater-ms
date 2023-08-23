c7ff23c6f73e629a966c88ac0b04c1b19e564f58
NorVirae <norbertmbafrank@gmail.com>
2023-08-23 14:49:53 +0100

    elastic search clone for dynamo db ops in specificity

---
+++ CreatedEventSnsHandler.cs
@@ -0,0 +1,65 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.SNSEvents;
+using Nest;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+namespace DynamoDbSnsUpdaterService
+{
+
+    public class CreatedEventSnsHandler
+    {
+        public async Task Handler(SNSEvent snsEvent)
+        {
+            var host = Environment.GetEnvironmentVariable("host");
+            var userName = Environment.GetEnvironmentVariable("userName");
+            var password = Environment.GetEnvironmentVariable("password");
+            var indexName = Environment.GetEnvironmentVariable("indexName");
+
+            var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName("eu-north-1"));
+            var table = Table.LoadTable(dbClient, "hotel-created-event");
+
+
+
+
+
+
+            var connSettings = new ConnectionSettings(new Uri(host));
+            connSettings.BasicAuthentication(userName, password);
+            connSettings.DefaultIndex(indexName);
+            connSettings.DefaultMappingFor<HotelCreatedEvent>(m => m.IdProperty(p => p.id));
+
+            var esclient = new Nest.ElasticClient(connSettings);
+
+            if (!(await esclient.Indices.ExistsAsync(indexName)).Exists)
+            {
+                await esclient.Indices.CreateAsync(indexName);
+            }
+
+            foreach (var eventRecord in snsEvent.Records)
+            {
+                var eventId = eventRecord.Sns.MessageId;
+                var foundItem = await table.GetItemAsync(eventId);
+
+                if (foundItem == null)
+                {
+                    await table.PutItemAsync(new Document
+                    {
+                        ["eventId"] = eventId
+                    });
+                }
+                var hotel = JsonSerializer.Deserialize<HotelCreatedEvent>(eventRecord.Sns.Message);
+
+
+                var result = await esclient.IndexDocumentAsync<HotelCreatedEvent>(hotel);
+                Console.WriteLine("Huli ha " + result);
+
+            }
+            Console.WriteLine("Got here and actually finished");
+
+        }
+    }
+}
---
+++ Models/HotelCreatedEvent.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamoDbSnsUpdaterService.Models
+{
+    public class HotelCreatedEvent
+    {
+        [DynamoDBHashKey("userId")]
+        public string userId { get; set; }
+
+        [DynamoDBRangeKey("id")]
+        public string id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+        public int Rating { get; set; }
+        public string CityName { get; set; }
+        public string FileName { get; set; }
+        public DateTime CreationDateTIme { get; set; }
+    }
+}
